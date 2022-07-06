ae8a848c373f6312f63868fb78c160d905ce99d1
Thanh An <vthanh.an57@gmail.com>
2022-07-06 09:45:32 +0700

    script for database table users

--- utils/database.sql
+++ utils/database.sql
@@ -3,3 +3,19 @@ CREATE TABLE [dbo].[sessions](
     [session] [nvarchar](max) NOT NULL,
     [expires] [datetime] NOT NULL
 )
+
+CREATE TABLE [users](
+    id INT NOT NULL IDENTITY(1,1),
+    username VARCHAR(255) NOT NULL,
+    email VARCHAR(255) UNIQUE,
+    password VARCHAR(255),
+	firstName NVARCHAR(255),
+	lastName NVARCHAR(255),
+	dob DATE,
+	job NVARCHAR(255),
+	address NVARCHAR(255),
+	phone VARCHAR(20),
+	facebook VARCHAR(255),
+	linkedin VARCHAR(255),
+	gender BIT
+)
